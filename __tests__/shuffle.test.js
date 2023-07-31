250ac247b82974ff0bb747a5926cf9cc1aaf7ff3
Joi Misenti <joipolloi1@gmail.com>
2023-07-30 21:07:17 -0400

    finished unit tests

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,19 @@
 const shuffle = require("../src/shuffle");
 
+let testDataArr = ["Optimus Prime", "Bumblebee", "Bender", "Baymax", "Wall-E", "Voltron"]
+
 describe("shuffle should...", () => {
-  // CODE HERE
+  test("return an array", () => {
+    const returnValue = shuffle(testDataArr)
+    expect(returnValue).toBeInstanceOf(Array);
+  })
+
+  it("return array of same length as argument", () => {
+    expect(shuffle(testDataArr)).toHaveLength(testDataArr.length);
+  })
+
+  test("contain all the same items originally in the array", () => {
+    expect(testDataArr).toEqual(expect.arrayContaining(shuffle(testDataArr)))
+  })
+  
 });
